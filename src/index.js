aaa3f5be380d2a3e101a1e7e7716972efef50f68
hiketra <emma.upton.95@gmail.com>
2019-12-02 07:30:49 +0000

    with basic routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import './App.css'
+import {BrowserRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom'
+import HomePage from './homepage.component'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HatsPage = () => (
+    <div>
+    <h1>Hats Page</h1>
+    </div>
+)
+
+function App() {
+    return(
+        <div>
+        <Switch>
+            <Route exact path ='/' component={HomePage} />
+            <Route exact path ='/hats' component={HatsPage} />
+        </Switch>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
