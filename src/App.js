e951b17e24b1df20595e200ce71eaecb544dd1ac
hiketra <emma.upton.95@gmail.com>
2020-01-04 03:24:17 +0000

    sign in firebase integration

--- src/App.js
+++ src/App.js
@@ -8,10 +8,34 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-function App() {
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
   return (
     <div>
-      <Header />
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -19,6 +43,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
